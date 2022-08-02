66501e783c51b23f351b676d43d2617f2f4035bd
Kevin Stowe <kevincstowe@gmail.com>
2022-08-02 09:47:06 -0400

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,4 +1,6 @@
 import streamlit as st
 st.title("Generate")
 
-st.write("The generator demo is currently being moved for security reasons - stay tuned for the updated version!")
+st.write("The generator demo is now available at: acg-demo.nlplab-dev.c.ets.org/concept2seq")
+
+st.write("For more on how to use this and other ACG demos at ETS, see <a href='https://confluence.ets.org/display/ACG/ACG+Demos'>our confluence page</a>")
